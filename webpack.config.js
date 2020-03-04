efe3f336dbfb0ec1ea23bbc0f4102504e6861fb8
Andrew Kurano <andrew.kurano@triggerglobal.com>
2020-03-03 16:24:08 -0800

    Added webpack config file and initial js file

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+let cssLoaders = [
+  "style-loader",
+  "css-loader",
+  "postcss-loader",
+  "sass-loader",
+  MiniCssExtractPlugin.loader
+]
+
+module.exports = {
+  mode: "development",
+  entry: {main: "./js/app.js"},
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        exclude: /node_modules/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [ [
+              "@babel/preset-env",
+              {
+                "targets": {
+                  "browsers": "iOS 10"
+                }
+              }
+            ]],
+            plugins: ["@babel/plugin-proposal-object-rest-spread"]
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: cssLoaders
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name:'[name].[ext]',
+          publicPath: './assets/imgs',
+          outputPath: './assets/imgs'
+        }
+      },
+      {
+        test: /\.(otf|ttf|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          name:'[name].[ext]',
+          publicPath: './assets/font',
+          outputPath: './assets/font'
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: "./build",
+      overlay: true,
+      hot: true
+  },
+  resolve: {
+    extensions: ['*', '.js', '.vue', '.json'],
+      alias: {
+      vue: "vue/dist/vue.min.js"
+    }
+  },
+}
