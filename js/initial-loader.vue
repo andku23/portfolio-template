768062ccb0144cf74af4c246f7ba2228f494f84d
Andrew Kurano <andrew.kurano@triggerglobal.com>
2020-03-04 19:02:47 -0800

    Started adding basic loader

--- js/initial-loader.vue
+++ js/initial-loader.vue
@@ -1,13 +1,36 @@
 <template>
     <div class = "fill-container initial-loader">
         <div class="background"/>
-        welcome
+        <div class = "loading-center">
+            <div class = "loading-text">
+                <div class = "text">{{this.loadingText[this.textIndex]}}</div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "InitialLoader"
+        name: "InitialLoader",
+        data: function(){
+            return {
+                loadingText: [
+                    "Text 1",
+                    "Text 2"
+                ],
+                textInterval: 4000,
+                textIndex: 0
+            }
+        },
+        mounted: function() {
+        },
+        methods: {
+            runLoadingText(){
+                setTimeout(() => {
+
+                }, this.textInterval);
+            }
+        }
     }
 </script>
 
@@ -17,6 +40,20 @@
         .background{
             @extend %absolute-fill;
             background-color:black;
+        }
+        .loading-center{
+            position:absolute;
+            top: 50%;
+            width: 100%;
+            .loading-text{
+                @extend %flex-parent-center;
+                top: 10vh;
+                .text{
+                    position: relative;
+                    text-align: center;
+                    color: white;
+                }
+            }
         }
     }
 </style>
